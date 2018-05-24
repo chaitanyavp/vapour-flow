60058f36c9302f494dc2e69a68792ca9eafe3d0f
chaitanyavp <chaitanya1998@gmail.com>
2018-05-23 21:45:04 -0400

    modified to make it scan 3x3 image

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ import tensorflow.contrib.eager as tfe
 
 
 def parse_csv(line):
-  example_defaults = [[0.], [0.], [0.], [0.], [0]]  # sets field types
+  example_defaults = [[0.], [0.], [0.], [0.], [0.], [0.], [0.], [0.], [0.], [0]]  # sets field types
   parsed_line = tf.decode_csv(line, example_defaults)
   # First 4 fields are features, combine into single tensor
-  features = tf.reshape(parsed_line[:-1], shape=(4,))
+  features = tf.reshape(parsed_line[:-1], shape=(9,))
   # Last field is the label
   label = tf.reshape(parsed_line[-1], shape=())
   return features, label
@@ -30,9 +30,10 @@ tf.enable_eager_execution()
 print("TensorFlow version: {}".format(tf.VERSION))
 print("Eager execution: {}".format(tf.executing_eagerly()))
 
-train_dataset_url = "http://download.tensorflow.org/data/iris_training.csv"
-train_dataset_fp = tf.keras.utils.get_file(fname=os.path.basename(train_dataset_url),
-                                           origin=train_dataset_url)
+# train_dataset_url = "http://download.tensorflow.org/data/iris_training.csv"
+# train_dataset_fp = tf.keras.utils.get_file(fname=os.path.basename(train_dataset_url),
+#                                            origin=train_dataset_url)
+train_dataset_fp = "train.csv"
 
 print("Local copy of the dataset file: {}".format(train_dataset_fp))
 
@@ -48,13 +49,12 @@ print("example features:", features[0])
 print("example label:", label[0])
 
 model = tf.keras.Sequential([
-  tf.keras.layers.Dense(10, activation="relu", input_shape=(4,)),  # input shape required
+  tf.keras.layers.Dense(10, activation="relu", input_shape=(9,)),  # input shape required
   tf.keras.layers.Dense(10, activation="relu"),
   tf.keras.layers.Dense(3)
 ])
 
 optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.01)
-
 
 ## Note: Rerunning this cell uses the same model variables
 
@@ -100,3 +100,39 @@ axes[1].set_xlabel("Epoch", fontsize=14)
 axes[1].plot(train_accuracy_results)
 
 plt.show()
+
+#-------------------------------------------------------------------
+
+test_fp = "test.csv"
+
+test_dataset = tf.data.TextLineDataset(test_fp)
+test_dataset = test_dataset.skip(1)             # skip header row
+test_dataset = test_dataset.map(parse_csv)      # parse each row with the funcition created earlier
+test_dataset = test_dataset.shuffle(1000)       # randomize
+test_dataset = test_dataset.batch(32)           # use the same batch size as the training set
+
+test_accuracy = tfe.metrics.Accuracy()
+
+#-------------------------------------------------------------------
+
+for (x, y) in test_dataset:
+  prediction = tf.argmax(model(x), axis=1, output_type=tf.int32)
+  test_accuracy(prediction, y)
+
+print("Test set accuracy: {:.3%}".format(test_accuracy.result()))
+
+class_ids = ["Iris setosa", "Iris versicolor", "Iris virginica"]
+
+predict_dataset = tf.convert_to_tensor([
+    [5.1, 3.3, 1.7, 0.5,],
+    [5.9, 3.0, 4.2, 1.5,],
+    [6.9, 3.1, 5.4, 2.1]
+])
+
+predictions = model(predict_dataset)
+
+for i, logits in enumerate(predictions):
+  class_idx = tf.argmax(logits).numpy()
+  name = class_ids[class_idx]
+  print("Example {} prediction: {}".format(i, name))
+
