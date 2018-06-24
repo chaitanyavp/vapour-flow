13a9eee6102483d7db752b442ad91833616e406f
chaitanyavp <chaitanya1998@gmail.com>
2018-06-23 23:33:50 -0400

    modularized several functions into methods

--- steam_data_regressor.py
+++ steam_data_regressor.py
@@ -19,7 +19,6 @@ class SteamDataRegressor:
         self._path = "steam_data.csv"
         self._y_name = "Time"
 
-        # Order is important for the csv-readers, so we use an OrderedDict here.
         self._defaults = collections.OrderedDict([
             ("Action", [""]),
             ("Adventure", [""]),
@@ -32,14 +31,13 @@ class SteamDataRegressor:
             ("Sports", [""]),
             ("Strategy", [""]),
             ("Time", [0.0])
-        ])  # pyformat: disable
-
+        ])
         self._types = collections.OrderedDict((key, type(value[0]))
                                 for key, value in self._defaults.items())
-
         self._csv_column_names = ['Action', 'Adventure', 'Casual', 'Indie',
                                   'Massively_Multiplayer', 'Racing', 'RPG',
                                   'Simulation', 'Sports', 'Strategy', 'Time']
+        self._model = None
 
     def decode_line(self, line):
         """Convert a csv line into a (features_dict,label) pair."""
@@ -57,10 +55,6 @@ class SteamDataRegressor:
 
     def in_training_set(self, line):
         """Returns a boolean tensor, true if the line is in the training set."""
-        # If you randomly split the dataset you won't get the same split in both
-        # sessions if you stop and restart training later. Also a simple
-        # random split won't work with a dataset that's too big to `.cache()` as
-        # we are doing here.
         bucket_id = tf.string_to_hash_bucket_fast(line, 1000000)
         # Use the hash bucket id as a random number that's deterministic per example
         return bucket_id < int(self._train_fraction * 1000000)
@@ -68,39 +62,61 @@ class SteamDataRegressor:
     def scale_down(self, features, labels):
         return features, labels / self._scale
 
+    def create_model(self):
+        category_vocab = ["True", "False"]
+        feature_columns = []
+        for col_name in self._csv_column_names[:-1]:
+            feature_columns.append(tf.feature_column.indicator_column(
+              tf.feature_column.categorical_column_with_vocabulary_list(
+                  key=col_name, vocabulary_list=category_vocab))
+            )
+        # Build  DNNRegressor, with 2x20-unit hidden layers, with featurecolumns
+        self._model = tf.estimator.DNNRegressor(
+          hidden_units=[20, 20], feature_columns=feature_columns)
+
+    def train_model(self, base_dataset):
+        train = (base_dataset.filter(self.in_training_set).map(self.decode_line)
+                 .cache())
+        train = train.map(self.scale_down)
+
+        def _train_helper():
+            return (train.shuffle(1000).batch(128)
+                    .repeat().make_one_shot_iterator().get_next())
+
+        self._model.train(input_fn=_train_helper, steps=self._steps)
+
+    def test_model(self, base_dataset):
+        test = (base_dataset.filter(lambda line: ~self.in_training_set(line))
+                .cache().map(self.decode_line))
+        test = test.map(self.scale_down)
+
+        def _test_helper():
+            return (test.shuffle(1000).batch(128)
+                    .make_one_shot_iterator().get_next())
+
+        # Evaluate how the model performs on data it has not yet seen.
+        eval_result = self._model.evaluate(input_fn=_test_helper)
+
+        # The evaluation returns a Python dictionary. The "average_loss" key
+        # holds the Mean Squared Error (MSE).
+        average_loss = eval_result["average_loss"]
+        # Convert MSE to Root Mean Square Error (RMSE).
+        print("\n" + 80 * "*")
+        print("\nRMS error for the test set: {:.0f}"
+            .format(self._scale * average_loss ** 0.5))
+
+        print()
+
     def main(self, argv):
         """Builds, trains, and evaluates the model."""
         assert len(argv) == 1
         base_dataset = (tf.data.TextLineDataset(self._path))
 
-        train = (base_dataset.filter(self.in_training_set).map(self.decode_line)
-                 .cache())
-
-        # Do the same for the test-set.
-        test = (base_dataset.filter(lambda line: ~self.in_training_set(line))
-                .cache().map(self.decode_line))
-
-        # Scale down labels.
-        train = train.map(self.scale_down)
-        test = test.map(self.scale_down)
-
-        # Build the training input_fn.
-        def input_train():
-            return (
-                # Shuffling with a buffer larger than the data set ensures
-                # that the examples are well mixed.
-                train.shuffle(1000).batch(128)
-                # Repeat forever
-                .repeat().make_one_shot_iterator().get_next())
-
         # Build the validation input_fn.
-        def input_test():
-            return (test.shuffle(1000).batch(128)
-                    .make_one_shot_iterator().get_next())
 
-        def eval_input_fn(features, labels, batch_size):
+        def _eval_helper(features, labels, batch_size):
             """An input function for evaluation or prediction"""
-            features=dict(features)
+            features = dict(features)
             if labels is None:
                 # No labels, use only features.
                 inputs = features
@@ -117,41 +133,14 @@ class SteamDataRegressor:
             # Return the dataset.
             return dataset
 
-        # The first way assigns a unique weight to each category. To do this you must
-        # specify the category's vocabulary (values outside this specification will
-        # receive a weight of zero). Here we specify the vocabulary using a list of
-        # options. The vocabulary can also be specified with a vocabulary file (using
-        # `categorical_column_with_vocabulary_file`). For features covering a
-        # range of positive integers use `categorical_column_with_identity`.
-        category_vocab = ["True", "False"]
-        feature_columns = []
-        for col_name in self._csv_column_names[:-1]:
-            feature_columns.append(tf.feature_column.indicator_column(
-              tf.feature_column.categorical_column_with_vocabulary_list(key=col_name,
-                vocabulary_list=category_vocab))
-            )
-
-        # Build a DNNRegressor, with 2x20-unit hidden layers, with the feature columns
-        # defined above as input.
-        model = tf.estimator.DNNRegressor(
-          hidden_units=[20, 20], feature_columns=feature_columns)
+        self.create_model()
+        assert self._model is not None
 
         # Train the model.
-        model.train(input_fn=input_train, steps=self._steps)
+        self.train_model(base_dataset)
 
-        # Evaluate how the model performs on data it has not yet seen.
-        eval_result = model.evaluate(input_fn=input_test)
-
-        # The evaluation returns a Python dictionary. The "average_loss" key holds the
-        # Mean Squared Error (MSE).
-        average_loss = eval_result["average_loss"]
-
-        # Convert MSE to Root Mean Square Error (RMSE).
-        print("\n" + 80 * "*")
-        print("\nRMS error for the test set: {:.0f}"
-            .format(self._scale * average_loss ** 0.5))
-
-        print()
+        # Test the model
+        self.test_model(base_dataset)
 
         predict_x = {
             'Action':    ["False", "False", "True"],
@@ -166,14 +155,10 @@ class SteamDataRegressor:
             'Strategy':  ["False", "True", "False"]
         }
 
-        # predictions = classifier.predict(
-        #     input_fn=lambda:eval_input_fn(predict_x,expected,
-        #                                         batch_size=batch_size))
-
         games = ["Skyrim", "Civ 6", "PUBG"]
-        predictions = model.predict(
-            input_fn=lambda: eval_input_fn(predict_x, None,
-                                           batch_size=128))
+        predictions = self._model.predict(
+            input_fn=lambda: _eval_helper(predict_x, None,
+                                          batch_size=128))
         for i, prediction in enumerate(predictions):
             print(" ", games[i] + ": ", prediction["predictions"][0] * self._scale / 30, "hours")
             # print(" ", prediction["predictions"][0], "mins")
